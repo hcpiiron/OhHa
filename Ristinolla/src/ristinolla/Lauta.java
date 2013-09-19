f54ead60e536f1d9516f8ab58d2d8f4106cb2241
Henri Piironen <piironen.henri@gmail.com>
2013-09-19 17:27:54 +0300

    lautaa ja testeja

--- Ristinolla/src/ristinolla/Lauta.java
+++ Ristinolla/src/ristinolla/Lauta.java
@@ -7,36 +7,99 @@ package ristinolla;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  *
  * @author hcpiiron
  */
 public class Lauta {
-    
-    private Map<Ruutu, Pelaaja> lauta;
-    private int koko;
+
+    private int[][] lauta;
     private List<Pelaaja> pelaajat;
-    
-    
-    public Lauta(int koko){
-        this.koko=koko;
-        this.lauta= new HashMap<>();
-        this.pelaajat= new ArrayList<>();
+    private int koko;
+    private int voittosuora = 3;
+
+    public Lauta(int koko) {
+        this.koko = koko;
+        this.lauta = new int[3][3]; //Testataan toimintaa aluksi 3x3 kokoisella kentällä
+        this.pelaajat = new ArrayList<>();
+
+        for (int i = 0; i < lauta.length; i++) {
+            for (int j = 0; j < lauta[i].length; j++) {
+                lauta[i][j] = 0;
+            }
+        } // Voittoja etsiessä "tyhjät ruudut" ovat nollia
     }
-    
-    public void lisaaPelaaja(Pelaaja pelaaja){
+
+    public void muutaVoittosuoraa(int pituus) {
+        this.voittosuora = pituus;
+    }
+
+    public void lisaaPelaaja(Pelaaja pelaaja) {
         pelaajat.add(pelaaja);
     }
 
     public List<Pelaaja> getPelaajat() {
         return pelaajat;
     }
-    
-    
-    
-    
+
+    public void asetaMerkki(int merkki, int x, int y) {
+        lauta[x][y] = merkki;
+    }
+
+    public int testaaVoittoVaaka() { // ketällä X merkataan 1, 0 merkataan 2, ja tyhjä ruutu 0. Palauttaa voittajan symbolin 
+        int counter = 0;
+        for (int i = 0; i < lauta.length; i++) {
+            for (int j = 0; j < lauta[i].length; j++) {
+                if (lauta[i][j] != 0) {
+                    if (j + voittosuora <= lauta[i].length) {
+                        int etsittava = lauta[i][j];
+                        counter++;
+                        for (int k = j + 1; k < j + voittosuora; k++) {
+                            if (lauta[i][k] == etsittava) {
+                                counter++;
+                            }
+                        }
+                        if (counter == voittosuora) {
+                            return etsittava;
+                        } else {
+                            counter = 0;
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    public int testaaVoittoPysty() {
+        int counter = 0;
+        for (int i = 0; i < lauta.length; i++) {
+            for (int j = 0; j < lauta[i].length; j++) {
+                if (lauta[i][j] != 0) {
+                    if (i + voittosuora <= lauta.length) {
+                        int etsittava = lauta[i][j];
+                        counter++;
+                        for (int k = i + 1; k < i + voittosuora; k++) {
+                            if (lauta[k][j] == etsittava) {
+                                counter++;
+                            }
+                        }
+                        if (counter == voittosuora) {
+                            return etsittava;
+                        } else {
+                            counter = 0;
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+
+    }
+
+    public void testaaVoittoVino() {
+    }
     
     
 }
--- Ristinolla/test/LautaTest.java
+++ Ristinolla/test/LautaTest.java
@@ -48,5 +48,71 @@ public class LautaTest {
         assertTrue(lauta.getPelaajat().contains(pelaaja));
     }
     
+    @Test
+    public void ekaVaakaRistille(){
+        lauta.asetaMerkki(1, 0, 0);
+        lauta.asetaMerkki(1, 0, 1);
+        lauta.asetaMerkki(1, 0, 2);
+        assertEquals(1, lauta.testaaVoittoVaaka());
+    }
+    
+    @Test
+    public void tokaVaakaNollalle(){
+        lauta.asetaMerkki(2, 1, 0);
+        lauta.asetaMerkki(2, 1, 1);
+        lauta.asetaMerkki(2, 1, 2);
+        assertEquals(2, lauta.testaaVoittoVaaka());
+    }
+    
+    @Test
+    public void tyhjallaVaakaTasapeli(){
+        assertEquals(0, lauta.testaaVoittoVaaka());
+    }
+    
+    @Test
+    public void muutamanMerkinTasapeli(){
+        lauta.asetaMerkki(2, 0, 2);
+        lauta.asetaMerkki(2, 2, 1);
+        lauta.asetaMerkki(1, 0, 0);
+        assertEquals(0, lauta.testaaVoittoVaaka());
+        assertEquals(0, lauta.testaaVoittoPysty());
+    }
+    
+    @Test
+    public void tyhjallaPytsyTasapeli(){
+        assertEquals(0, lauta.testaaVoittoPysty());
+    }
+    
+    @Test
+    public void ekaPystyRistille(){
+        lauta.asetaMerkki(1, 0, 0);
+        lauta.asetaMerkki(1, 1, 0);
+        lauta.asetaMerkki(1, 2, 0);
+        assertEquals(1, lauta.testaaVoittoPysty());
+    }
+    
+    @Test
+    public void tokaPystyNollalle(){
+        lauta.asetaMerkki(2, 0, 1);
+        lauta.asetaMerkki(2, 1, 1);
+        lauta.asetaMerkki(2, 2, 1);
+        assertEquals(2, lauta.testaaVoittoPysty());
+    }
+    
+    @Test
+    public void tilanne(){
+        lauta.asetaMerkki(1, 0, 0);
+        lauta.asetaMerkki(1, 0, 1);
+        lauta.asetaMerkki(1, 1, 0);
+        lauta.asetaMerkki(1, 2, 0);
+        lauta.asetaMerkki(2, 1, 1);
+        lauta.asetaMerkki(2, 1, 2);
+        assertEquals(0, lauta.testaaVoittoVaaka());
+        assertEquals(1, lauta.testaaVoittoPysty());
+    }
+    
+    
+    
+    
     
 }
